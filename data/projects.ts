ee00932017bd90144108b02868813958923b5720
Herbert <herberthtk100@gmail.com>
2025-10-12 20:32:19 +0300

    Upgrade and cleanup

--- data/projects.ts
+++ data/projects.ts
@@ -8,65 +8,6 @@ export interface Projects {
   iconList: string[];
 }
 
-// export const projects = [
-//   {
-//     id: 1,
-//     title: "3D Solar System Planets to Explore",
-//     des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
-//     img: "/assets/p1.svg",
-//     iconLists: [
-//       "/assets/re.svg",
-//       "/assets/tail.svg",
-//       "/assets/ts.svg",
-//       "/assets/three.svg",
-//       "/assets/fm.svg",
-//     ],
-//     link: "/assets/ui.earth.com",
-//   },
-//   {
-//     id: 2,
-//     title: "Yoom - Video Conferencing App",
-//     des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
-//     img: "/assets/p2.svg",
-//     iconLists: [
-//       "/assets/next.svg",
-//       "/assets/tail.svg",
-//       "/assets/ts.svg",
-//       "/assets/stream.svg",
-//       "/assets/c.svg",
-//     ],
-//     link: "/assets/ui.yoom.com",
-//   },
-//   {
-//     id: 3,
-//     title: "AI Image SaaS - Canva Application",
-//     des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
-//     img: "/assets/p3.svg",
-//     iconLists: [
-//       "/assets/re.svg",
-//       "/assets/tail.svg",
-//       "/assets/ts.svg",
-//       "/assets/three.svg",
-//       "/assets/c.svg",
-//     ],
-//     link: "/assets/ui.aiimg.com",
-//   },
-//   {
-//     id: 4,
-//     title: "Animated Apple Iphone 3D Website",
-//     des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
-//     img: "/assets/p4.svg",
-//     iconLists: [
-//       "/assets/next.svg",
-//       "/assets/tail.svg",
-//       "/assets/ts.svg",
-//       "/assets/three.svg",
-//       "/assets/gsap.svg",
-//     ],
-//     link: "/assets/ui.apple.com",
-//   },
-// ];
-
 export const projects: Projects[] = [
   {
     title: "Sourcegraph",
